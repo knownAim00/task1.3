28696736e07ac2b579b9bb3227e7e62d99694bf5
knownAim00 <kostyaavakyan3@gmail.com>
2025-03-24 14:02:36 +0200

    Delete TASK directory

--- TASK/script.js
+++
@@ -1,8 +0,0 @@
-function handleClick(event) {
-    document.getElementById("myForm").addEventListener("submit", function (event) {
-        if (!this.checkValidity()) {
-            return;
-        }
-        event.preventDefault();
-    });
-}
